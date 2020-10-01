592b19512e9a03d0621bf41a25cfa7a81cdb40b4
Wizmann <mail.kuuy@gmail.com>
2020-10-01 15:04:36 +0800

    add cptouch

---
+++ cptouch.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+#coding=utf-8
+
+import os
+import sys
+import shutil
+
+exemplars_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), 'exemplars')
+
+exts = [
+    (['cc', 'cpp', 'cxx'], 'cpp'),
+    (['py'], 'py'),
+]
+
+d = {}
+
+for keys, value in exts:
+    for key in keys:
+        d[key] = value
+
+filename = sys.argv[1]
+ext = filename.split('.')[-1]
+
+if ext in d:
+    ext = d[ext]
+else:
+    assert False
+
+example_file = os.path.join(exemplars_path, 'example.' + ext)
+shutil.copy(example_file, filename)
---
+++ exemplars/example.cpp
@@ -0,0 +1,56 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <map>
+#include <set>
+#include <deque>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+typedef long long llint;
+
+const int INF = 0x3f3f3f3f;
+const int INFLL = 0x3f3f3f3f3f3f3f3fLL;
+
+void print() { cout << "\n"; }
+
+template <typename...T, typename X>
+void print(X&& x, T... args) { cout << x << " "; print(args...); }
+
+int input() { return 0; }
+
+template <typename...T, typename X>
+int input(X& x, T&... args) {
+    if (!(cin >> x)) return 0;
+    return input(args...) + 1;
+}
+
+int main()
+{
+    int n;
+    while (input(n)) {
+        int a = -1;
+        string b = "-1";
+        char c = -1;
+        float d = -1;
+        
+        assert(input(a, b, c, d) == n);
+        print(a, b, c, d);
+    }
+
+    return 0;
+}
+
+/*
+test input
+4
+1 2 3 4
+3
+1 2 3
+*/
