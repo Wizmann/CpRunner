82e602c817c3e2116e7ac75041923f2e37387e24
wizmann <mail.kuuy@gmail.com>
2023-10-21 14:25:02 +0800

    update py2/3 exemplars

--- exemplars/example.py
+++ exemplars/example.py
@@ -1,5 +1,13 @@
 #coding=utf-8
 
+import sys
+if sys.version_info[0] == 3:
+    raw_input = input
+    xrange = range
+    map_ = map
+    def map(*args, **kwargs):
+        return list(map_(*args, **kwargs))
+
 T = int(raw_input())
 
 for case_ in xrange(T):
